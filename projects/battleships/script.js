f9f1a11f6c1ff42367a02343ea4ed6c895c9d3e3
DannyLenk <danylenko.14@gmail.com>
2021-12-11 09:52:09 +0200

    updated script.js
    built fire method

--- projects/battleships/script.js
+++ projects/battleships/script.js
@@ -6,14 +6,48 @@ let view = {
    },
    displayHit: function(location) {
       document.getElementById(location).setAttribute('class', 'hit');
-      this.displayMessage('HIT!');
+      this.displayMessage("HIT!");
    },
    displayMiss: function(location) {
       document.getElementById(location).setAttribute('class', 'miss');
-      this.displayMessage('You missed');
+      this.displayMessage("You missed");
    }
 };
 
 let model = {
-   
-}
+   boardSize: 7,
+   shipSize: 3,
+   shipsNum: 3,
+   shipsSunk: 0,
+   ships: [
+      { locations: ['00', '01', '02'], hits: ['', '', ''] },
+      { locations: ['20', '21', '22'], hits: ['', '', ''] },
+      { locations: ['10', '11', '12'], hits: ['', '', ''] }
+   ],
+
+   fire: function(guess) {
+      for (let i = 0; i < this.shipsNum; i++) {
+         let ship = this.ships[i];
+         let index = ship.locations.indexOf(guess);
+         if (index >= 0) {
+            ship.hits[index] = 'hit';
+            view.displayHit(guess);
+            this.isSunk(ship);
+            return true;
+         }
+      }
+      view.displayMiss(guess);
+      return false;
+   },
+
+   isSunk: function(ship) {
+      for (let i = 0; i < this.shipSize; i++) {
+         if (ship.hits[i] !== 'hit') {
+            return false;
+         }
+      }
+      this.shipsSunk++;
+      view.displayMessage('You sank my battleship!');
+      return true;
+   }
+};
