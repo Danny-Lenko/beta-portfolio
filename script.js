097177285f79c11bc78e2e666bb7f5bcfcbddead
DannyLenk <danylenko.14@gmail.com>
2021-12-11 12:04:30 +0200

    updated index.html, script.js
    added the battleship game to the main page

--- script.js
+++ script.js
@@ -3,5 +3,9 @@
 document.querySelector('#showAllBtn').addEventListener('click', showAllContent);
 
 function showAllContent() {
-   document.querySelector(".hidden1").removeAttribute("hidden");
+   let hiddenList = document.getElementsByClassName('hidden1');
+   for (let i = 0; i < hiddenList.length; i++) {
+      let hiddenElement = hiddenList[i];
+      hiddenElement.removeAttribute('hidden');
+   }
 }
