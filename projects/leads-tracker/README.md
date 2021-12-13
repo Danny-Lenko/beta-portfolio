8fac68084e734bcc5f4df4302e99350a19e3baf9
DannyLenk <danylenko.14@gmail.com>
2021-12-13 10:27:34 +0200

    added the leads tracker extension

---
+++ projects/leads-tracker/README.md
@@ -0,0 +1,30 @@
+# Chrome Extension - Leads Tracker
+
+### What I learned
+
+1) the Utility Classes "gap" property to use between flex and grid elements (html customizable variable)
+
+```css
+.flex {
+   display: flex;
+   gap: var(--gap, 1.6rem);
+}
+.grid {
+   display: grid;
+   gap: var(--gap, 1.6rem);
+}
+```
+
+### Continued development
+
+* CSS grid;
+
+*Use this section to outline areas that you want to continue focusing on in future projects. These could be concepts you're still not completely comfortable with or techniques you found useful that you want to refine and perfect.
+
+### Useful resources
+
+- [JSON.stringify()](https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify) - This helped me with the Local Storage interaction.
+- [URL for the extension](https://stackoverflow.com/questions/6132018/how-can-i-get-the-current-tab-url-for-chrome-extension) - The first answer describes the way to get the current URL for a Chrome Extension.
+
+- [Git + GitHub](https://www.youtube.com/watch?v=RGOj5yH7evk) - the basic git commands (Youtube).
+- [The Markdown Guide](https://www.markdownguide.org/) - for more help with writing markdown (Article).
---
+++ projects/leads-tracker/index.js
@@ -0,0 +1,59 @@
+"use strict"
+
+let interaction = {
+   myLeads: [],
+
+   saveInput: function() {
+      const inputEl = document.querySelector('#input-el');
+      const inputValue = inputEl.value;
+
+      if (inputValue) {
+         interaction.myLeads.push(inputValue);
+      }
+      localStorage.setItem('myLeads', JSON.stringify(interaction.myLeads));
+      inputEl.value = '';
+      view.parseLeads();
+   },
+
+   deleteLeads: function() {
+      const ulEl = document.querySelector('#ul-el');
+      interaction.myLeads = [];
+      localStorage.clear();
+      ulEl.innerHTML = '';
+   },
+
+   saveCurrentTab: function() {
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+         interaction.myLeads.push(tabs[0].url);
+         localStorage.setItem('myLeads', JSON.stringify(interaction.myLeads));
+         view.parseLeads();
+      })
+   }
+};
+
+let view = {
+   parseLeads: function() {
+      let restoredLeads = JSON.parse(localStorage.getItem('myLeads'));
+      if (restoredLeads) {
+         interaction.myLeads = restoredLeads;
+      }
+      this.renderLeads(interaction.myLeads);
+   },
+
+   renderLeads: function(value) {
+      const ulEl = document.querySelector('#ul-el');
+      let leadsList = '';
+      for (let i = 0; i < value.length; i++) {
+         leadsList += `<li> <a href="${value[i]}" target="_blank"> ${value[i]} </a> <li>`;
+      }
+      ulEl.innerHTML = leadsList;
+   }
+};
+
+function init() {
+   document.querySelector('#input-btn').addEventListener('click', interaction.saveInput);
+   document.querySelector('#delete-btn').addEventListener('dblclick', interaction.deleteLeads);
+   document.querySelector('#tab-btn').addEventListener('click', interaction.saveCurrentTab);
+   view.parseLeads();
+}
+window.onload = init;
